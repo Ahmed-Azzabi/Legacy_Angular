ba62b51d533f4a516e245ecdfeea05230341ed5a
Sarah Kcm <100924437+sarahkcm@users.noreply.github.com>
2022-06-14 18:39:31 +0200

    navigate Products

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -12,6 +12,11 @@ import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { CartElementComponent } from './cart-element/cart-element.component';
 import { ProductByCategoryComponent } from './product-by-category/product-by-category.component';
 import { UserComponent } from './user/user.component';
+import { NavigateProductsComponent } from './navigate-products/navigate-products.component';
+import { BeautyComponent } from './navigate-products/beauty/beauty.component';
+import { ElectronicsComponent } from './navigate-products/electronics/electronics.component';
+import { HomeAndGardenComponent } from './navigate-products/home-and-garden/home-and-garden.component';
+import { JewerlyWatchesComponent } from './navigate-products/jewerly-watches/jewerly-watches.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +29,12 @@ import { UserComponent } from './user/user.component';
     ShoppingCartComponent,
     CartElementComponent,
     ProductByCategoryComponent,
-    UserComponent
+    UserComponent,
+    NavigateProductsComponent,
+    BeautyComponent,
+    ElectronicsComponent,
+    HomeAndGardenComponent,
+    JewerlyWatchesComponent
   ],
   imports: [
     BrowserModule,
