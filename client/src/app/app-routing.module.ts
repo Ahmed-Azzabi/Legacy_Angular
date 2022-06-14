e0b3a1d68e5618493844fa98df6db6af3e87cd88
Ahmed-Azzabi <azzabiahmed98@gmail.com>
2022-06-14 18:49:01 +0100

    admin routes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminAddProductComponent } from './admin-add-product/admin-add-product.component';
+import { AdminSeeOrdersComponent } from './admin-see-orders/admin-see-orders.component';
+import { AdminUpdateProductComponent } from './admin-update-product/admin-update-product.component';
+import { AdminViewComponent } from './admin-view/admin-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'addproduct', component: AdminAddProductComponent },
+  { path: 'seeorders', component: AdminSeeOrdersComponent },
+  { path: 'updateproduct', component: AdminUpdateProductComponent },
+  { path: 'adminview', component: AdminViewComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
