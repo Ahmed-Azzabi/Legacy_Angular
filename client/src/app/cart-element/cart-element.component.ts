fd2541afb3f8e0bb0f81194d6303985e79e856d9
mehdi <100924042+mehdiEnnabli@users.noreply.github.com>
2022-06-15 13:36:43 +0100

    check the token

--- client/src/app/cart-element/cart-element.component.ts
+++ client/src/app/cart-element/cart-element.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { CartsService } from '../services/carts.service';
 @Component({
   selector: 'app-cart-element',
   templateUrl: './cart-element.component.html',
   styleUrls: ['./cart-element.component.css']
 })
 export class CartElementComponent implements OnInit {
+  cart: any = {};
+  cartForm: FormGroup|any;
+  constructor(private cartsService:CartsService,
+              private formBuilder: FormBuilder,
+              ) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.cartForm = this.formBuilder.group({
+      teamOne: [''],
+      teamTwo: [''],
+      scoreOne: [''],
+      scoreTwo: [''],
+      logo1:[''],
+      logo2:['']
+    })
+  }
+  addCart() {
+    console.log('Here my cart object', this.cart );
+    this.cartsService.addCart(this.cart).subscribe(
+      () => {
+        console.log('Match added successfully');
+        // this.router.navigate(['home']);
+      }
+    )
   }
 
 }
