7a782b1ea71d48946dc75d28157af9991e11af49
Ahmed-Azzabi <azzabiahmed98@gmail.com>
2022-06-18 10:42:34 +0100

    support ticket side functional

--- client/src/app/support-tickets/support-tickets.component.ts
+++ client/src/app/support-tickets/support-tickets.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { SupportService } from '../services/support.service';
 
 @Component({
   selector: 'app-support-tickets',
   templateUrl: './support-tickets.component.html',
-  styleUrls: ['./support-tickets.component.css']
+  styleUrls: ['./support-tickets.component.css'],
 })
 export class SupportTicketsComponent implements OnInit {
+  allTickets: any;
+  constructor(private supportService: SupportService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.supportService.getAllTickets().subscribe((data) => {
+      this.allTickets = data;
+      console.log(data, 'tickets');
+    });
   }
-
 }
